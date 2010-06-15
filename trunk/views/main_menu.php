5c16b2bdd757042ee040034616812e2114eb23ae
Astarta.V <Astarta.V@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-15 13:55:47 +0000

    design!

--- trunk/views/main_menu.php
+++ trunk/views/main_menu.php
@@ -1,9 +1,11 @@
-<div id='mainmenu'>
-    <span> | </span>
-    <a href='#' onclick="sendm('userprofile');" >Вы в игре как <?php echo $data['user']['user_login']; ?></a><span> | </span>
-    <a href='#' onclick="sendm('logout');" >Выход ИЗ игры</a><span> | </span>
-    <a href='./user.php' target="_blank" >Энциклопедия</a><span> | </span>
-    <a href='#' onclick="sendm('news');" >Новости проекта</a><span> | </span>
-    <a href='#' onclick="sendm('galaxy');" >Игра</a><span> | </span>
-    <a href='#' onclick="sendm('battle');" >Бой</a><span> | </span>
+<div id="left_menu"></div><div id='mainmenu'>
+    
+    
+    <a href='./user.php' target="_blank" >Энциклопедия</a>&nbsp;&nbsp;&nbsp;
+    <a href='#' onclick="sendm('news');" >Новости проекта</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href='#' onclick="sendm('galaxy');" >Игра</a><span>&nbsp;&nbsp;&nbsp;
+    <a href='#' onclick="sendm('battle');" >Бой</a><span> &nbsp;&nbsp;&nbsp;
+    <div id="user_profile">Вы в игре как <a href='#' onclick="sendm('userprofile');" ><?php echo $data['user']['user_login']; ?></a>
+    (<span id="exit"><a href='#' onclick="sendm('logout');" >выйти</a></span>)</div>
 </div>
+<div id="right_menu"></div>
