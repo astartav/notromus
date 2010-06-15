c44f161de4200e8bf0645ca93cfa24b1566af0a2
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-15 09:18:15 +0000

    добавлен очередной вид

---
+++ trunk/views/user_mode.php
@@ -0,0 +1,7 @@
+﻿<div id='usermode'>
+	<span> </span>
+	<a href='#' onclick="sendi('userconfig','');" >userconfig</a><span> </span>
+	<a href='#' onclick="sendi('forum','');" >forum</a><span> </span>
+	<a href='#' onclick="sendi('gallery','');" >gallery</a><span> </span>
+	<a href='#' onclick="sendi('about',' ');" >about</a><span> </span>
+</div>
