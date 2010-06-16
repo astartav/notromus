28d4a6ef80670f30aed0d81b42c103ea265caab9
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-16 07:08:31 +0000

    добавлена реализация вью через перехват выходного потока. добавлено пробное многоуровневое вью game structure

---
+++ trunk/views/game_structure.php
@@ -0,0 +1,8 @@
+<div id='monitor'></div>
+<div id='controlarea'>
+    <?php include_once("main_menu.php");?>
+    <?php include_once("mode_menu.php");?>
+    <div id='controlbar'></div>
+    <?php include_once("chat.php");?>
+</div>
+<div id='info'></div>
