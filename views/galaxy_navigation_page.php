fe8fd119a6f36ec59edef12116581a3c5790c678
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-21 14:16:01 +0000

    поправка в механизме вьюверов для просмотра навигации

---
+++ views/galaxy_navigation_page.php
@@ -0,0 +1,6 @@
+<?php include_once("main_menu.php"); ?>
+<div id='modearea'>
+    <?php include_once("galaxy_navigation.php"); ?>
+</div>
+<div id='messages'></div>
+<?php include_once("chat.php"); ?>
--- views/game_structure.php
+++ views/game_structure.php
@@ -1,27 +1,6 @@
-<?php //include_once("main_menu.php"); ?>
-
-<div id="left_menu"></div>
-<div id='mainmenu'>
-    <a href='./user.php' target="_blank" >Энциклопедия</a>
-    <a href='#' onclick="sendm('news');" >Новости проекта</a>
-    <a href='#' onclick="sendm('galaxy');" >Игра</a>
-    <a href='#' onclick="sendm('battle');" >Бой</a>
-    <div id="user_profile">
-        Вы в игре как <a href='#' onclick="sendm('userprofile');" ><?php echo $data['user'][0]['user_login']; ?></a>(<span id="exit"><a href='#' onclick="sendm('logout');" >выйти</a></span>)
-    </div>
-</div>
-<div id="right_menu"></div>
-
+<?php include_once("main_menu.php"); ?>
 <div id='modearea'></div>
-
 <div id='messages'></div>
-<div id='chat'>
-    <div id='chat_monitor'></div>
-    <input id='chat_input' type='text' value=''/>
-    <input id='chat_timestamp' type='hidden' value='0' />
-    <input id='chat_button' type='button' value='send' onclick="sendw('chat','chat_input;chat_timestamp');" />
-</div>
-
-<?php //include_once("chat.php"); ?>
+<?php include_once("chat.php"); ?>
 
 
