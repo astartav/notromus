dfa0cad7b5535067c11614f3ac655120762c8fa6
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-21 20:02:18 +0000

    починен просмотр и создание персонажа, добавлены вью для локаций рынок и ремонтный двор

--- trunk/views/main_menu.php
+++ trunk/views/main_menu.php
@@ -4,6 +4,9 @@
     <a href='#' onclick="sendm('news');" >Новости проекта</a>
     <a href='#' onclick="sendm('galaxy');" >Игра</a>
     <a href='#' onclick="sendm('battle');" >Бой</a>
-    <div id="user_profile">Вы в игре как <a href='#' onclick="sendm('userprofile');" ><?php echo $data['user'][0]['user_login']; ?></a>
-    (<span id="exit"><a href='#' onclick="sendm('logout');" >выйти</a></span>)</div>
+    <div id="user_profile">
+        Вы в игре как <a href='#' onclick="sendm('userprofile');" ><?php echo $data['user'][0]['user_login']; ?></a>(<span id="exit"><a href='#' onclick="sendm('logout');" >выйти</a></span>)<br>
+        Ваш <a href='#' onclick="sendm('person');" >персонаж</a><br>
+    </div>
+
 </div><div id="right_menu"></div>
--- trunk/views/show_person.php
+++ trunk/views/show_person.php
@@ -1,13 +1,13 @@
 <div id="monitor">
 <h2>Персонаж - просмотр характеристик</h2>
 <form>
-    фракция: <?php echo $data['person']['fraction_id']; ?><br />
-    имя персонажа:<?php echo $data['person']['name']; ?><br />
-    описание:<?php echo $data['person']['description']; ?><br />
-    денежный счет:<?php echo $data['person']['account']; ?><br />
-    игровые очки:<?php echo $data['person']['score']; ?><br />
-    статус:<?php echo $data['person']['status']; ?><br />
-    игровой опыт:<?php echo $data['person']['experience']; ?><br />
-    ранг:<?php echo $data['person']['rang']; ?><br />
+    фракция: <?php echo $data['person'][0]['person_fraction_id']; ?><br />
+    имя персонажа:<?php echo $data['person'][0]['person_name']; ?><br />
+    описание:<?php echo $data['person'][0]['person_description']; ?><br />
+    денежный счет:<?php echo $data['person'][0]['person_account']; ?><br />
+    игровые очки:<?php echo $data['person'][0]['person_score']; ?><br />
+    статус:<?php echo $data['person'][0]['person_status']; ?><br />
+    игровой опыт:<?php echo $data['person'][0]['person_experience']; ?><br />
+    ранг:<?php echo $data['person'][0]['person_rang']; ?><br />
 </form>
 </div>
