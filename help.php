3df6a453147cd2d09a072ea90d3032c609f5e9c2
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-24 02:10:36 +0000

    добавлена генерация ссылок на лету + кеширование результата

--- help.php
+++ help.php
@@ -1,4 +1,7 @@
-<?php  session_start(); ?>
+<?php  session_start();
+$memcache = new Memcache;
+$memcache->connect('localhost', 11211) or die ("Could not connect");
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
@@ -8,8 +11,6 @@
             <script src="js/help.js" ></script>
     </head>
     <body>
-        <div id="main">
-
         <?php
             $helpname="";
 
@@ -35,17 +36,36 @@
             } else {
                 $helpname="main";
             }
-            $fh=fopen("help/".$helpname.".php",'r') or die($php_errormsg);
-            $s="";
-            while(! feof($fh)) {
-                $s = fgets($fh);
-                if($s) {
-                  echo preg_replace($patterns, $replacements, $s);
-                }
-            }
-            fclose($fh) or die($php_errormsg);
-        ?>
 
-        </div>
+            ?>
+            <div id='menu'>
+                <?php echo join(" | ", $replacements); ?>
+            </div>
+            <div id="main">
+            <?php
+
+            $value = $memcache->get($helpname);
+            if($value) {
+                echo $value;
+            } else {
+                if( @file_exists( "help/".$helpname.".php")) {
+                $fh=fopen("help/".$helpname.".php",'r');// or die($php_errormsg);
+                if( $fh ) {
+                    $all_s="";
+                    $s="";
+                    while(! feof( $fh )) {
+                        $s = fgets( $fh );
+                        if($s) {
+                            $all_s .= preg_replace($patterns, $replacements, $s);
+                        }
+                    }
+                fclose( $fh );// or die($php_errormsg);
+                $memcache->set($helpname,$all_s,false, 600) or die ("Failed to save data at the server");
+                echo $all_s;
+                }
+                } else echo "<h3>not found!</h3>";
+            }
+            ?>
+            </div>
     </body>
 </html>
