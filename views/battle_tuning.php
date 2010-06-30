c2344271143208fa5191e554e9693086c3e93c0c
gymlyg <gymlyg@25f64242-b485-11dd-89c7-03d08fec6209>
2010-06-30 17:47:17 +0000

    немного изменений для битвы

---
+++ views/battle_tuning.php
@@ -0,0 +1,4 @@
+<div style='position:relative'>
+    <div id='trum' style='position: absolute; overflow: auto; top: 495px; left: 10px; height: 90px; width: 780px; background-color: #eeeeee; border: 1px #ff0000 solid;'>
+    </div>
+</div>
---
+++ views/navi_tuning.php
@@ -0,0 +1,4 @@
+<div style='position:relative'>
+    <div id='trum' style='position: relative; overflow: auto;height: 90px; width: auto; background-color: #eeeeee; border: 1px #ff0000 solid;'>
+    </div>
+</div>
